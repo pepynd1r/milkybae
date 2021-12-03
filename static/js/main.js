adc37ed27ca4cbd78d784d98be3482b6b53cb088
pepo <pepynd1r@gmail.com>
2021-12-03 19:32:40 +0200

    dasda

--- static/js/main.js
+++ static/js/main.js
@@ -13,7 +13,12 @@
 $( document ).ready(function() {
   console.log( "ready!" );
 
+  function isFacebookApp() {
+    var ua = navigator.userAgent || navigator.vendor || window.opera;
+    return (ua.indexOf("FBAN") > -1) || (ua.indexOf("FBAV") > -1);
+}
 
+isFacebookApp();
 
   var video = $('#myVideo');
   if ((window.screen.width / window.screen.height) < 1.77){
